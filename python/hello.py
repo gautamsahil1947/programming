e6c441a088fd5e5ec3c20e0717536c788ae51eac
gautamsahil1947 <gautamsahil1947@gmail.com>
2022-11-15 16:58:14 +0530

    ok

---
+++ python/hello.py
@@ -0,0 +1,5 @@
+print("hello, world")
+# the first programm
+
+# learning leading to knowledge is a waste and that leading to action can make you a fortunate individual
+                                                                                                    # ----jim Rohn

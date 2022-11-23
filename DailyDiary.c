10a8952772e8dd8c6317d49a34e07db36d15fdca
gautamsahil1947 <gautamsahil1947@gmail.com>
2022-11-23 16:12:55 +0530

    ok

--- DailyDiary.c
+++ DailyDiary.c
@@ -3,7 +3,32 @@ this file is the logbook of sahil gautam that is me
 the standard format for the entries is:-
 and the latest entry will be at the first. remember, money has some value but time .....
 
+this is the tasklist for the day for the week or for the year.
+    full electrical
+    full scratch to memory revision and also taking notes of the same 
+    remake the resume now according to the real goals and the real things you want(badly)
+    work on it and go to the gym daily .
+    get a time table made and follow it
+    make notes from jim rohn stuff .
+    go for it badly and involve all the friends in it.
+    also drop the shut mouth or the offended things.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 date and time  started                     stuff done                            date and time when the stuff is finished
 
+
+Wed, Nov 23, 2022 12:28:00 PM             html report for CA                               Wed, Nov 23, 2022  3:48:09 PM
 Tue, Nov 22, 2022  8:40:04 PM         created the logbook                                  Tue, Nov 22, 2022  8:41:36 PM        */

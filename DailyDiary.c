65f5780202755f089438d164a79bacc8c449848e
gautamsahil1947 <gautamsahil1947@gmail.com>
2022-11-24 08:40:56 +0530

    let's go

--- DailyDiary.c
+++ DailyDiary.c
@@ -28,7 +28,21 @@ this is the tasklist for the day for the week or for the year.
 
 
 date and time  started                     stuff done                            date and time when the stuff is finished
+                                          
+                                          sleep at 7 am 
+                                          6 am to 6 30 am meditation
+                                          5am to 6 am gym
+                                          running from 4:30am to 5:00am
+                                          electrical full revision
+                                          learn c language till arrays
+                                          make the scratch project
+                                          dinner
+                                          python test from 5:30 pm to 7:30 pm
+                                          revise maths
+                                          make che presentation
 
 
+
+Thu, Nov 24, 2022  6:00:28 AM            python test codetantra                            
 Wed, Nov 23, 2022 12:28:00 PM             html report for CA                               Wed, Nov 23, 2022  3:48:09 PM
 Tue, Nov 22, 2022  8:40:04 PM         created the logbook                                  Tue, Nov 22, 2022  8:41:36 PM        */

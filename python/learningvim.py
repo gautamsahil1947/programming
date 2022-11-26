03a4c2eb1f13ff698c1fdc39651281767b0bc8b7
gautamsahil1947 <gautamsahil1947@gmail.com>
2022-11-26 13:45:47 +0530

    this is another commit from command line

---
+++ python/learningvim.py
@@ -0,0 +1,12 @@
+''' this is vim editor and it is cool in a number of ways 
+        the first one is that it does not suggest you stuff so you have to learn all on your own 
+        the second is that it doesn't auto complete the syntax like the parantheses.
+        the third one is that it is simple yet powerfull and if you know git then a terminal would be enough for your coding life
+'''
+n = int(input())  
+for i in range(n): 
+    for j in range(n):
+        print("#", end = " ")
+    print('\r')
+    
+

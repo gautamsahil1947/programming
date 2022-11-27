e42d7a6992e7594d4bc811818fcfde826a168c16
gautamsahil1947 <gautamsahil1947@gmail.com>
2022-11-27 11:41:38 +0530

    python notes

---
+++ python/notes/one.py
@@ -0,0 +1,56 @@
+# this is the first part of the notes.
+''' these notes are based on smartwork and not on plain hard work that is 
+    it is tried to get as much output as possible in as less time as 
+    possible without compromising the quality of learning. '''
+
+# python is an interpreted high level language for general purpose programming.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

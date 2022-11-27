d33aa238c83fbcb210c957a8da8a7a397aac2a95
gautamsahil1947 <gautamsahil1947@gmail.com>
2022-11-27 14:28:16 +0530

    c programm

--- c/mario.c
+++ c/mario.c
@@ -1,15 +1,3 @@
-/*
-# include <stdio.h>
-int main (void)
-{
-	int n;
-	printf("enter the number: );
-	scanf("%i" &n);
-	printf("the number is: %i\n", n);
-}
-// the code above has a lot of bugs like the , in the line number 6 is missing
-// and the second thing is that the "" are not in pair in the line number 5
-*/
 # include <stdio.h>
 int main (void)
 {
@@ -17,7 +5,7 @@ int main (void)
 	int n;
 	printf("enter the number: ");
 	scanf("%i", &n);
-	printf("the number is: %i", n);
+	printf("the number is: %i\n", n);
 
 
 

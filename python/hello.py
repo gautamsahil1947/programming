aebc96572b4b04cd69a17d60bccef7d7a27ab955
sahil <gautamsahil1947@gmail.com>
2022-12-07 22:47:12 +0530

    cleaning and organising

--- python/hello.py
+++
@@ -1,5 +0,0 @@
-print("hello, world")
-# the first programm
-
-# learning leading to knowledge is a waste and that leading to action can make you a fortunate individual
-                                                                                                    # ----jim Rohn
